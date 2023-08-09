e98bb5d693561da055c3ed795b58dd66d0892e1b
mxodaiy <m.khodaiy96@gmail.com>
2023-08-09 09:44:37 +0330

    added single game function

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function getComputerChoice(){
     i = Math.floor(Math.random()*3);
     return choices[i]
 }
+
+function battle(computerSelection, playerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (computerSelection === playerSelection){
+        return "Draw!"
+    } else if ((playerSelection === "rock" && computerSelection === "scissors")
+    || (playerSelection === "paper" && computerSelection === "rock")
+    || (playerSelection === "scissors" && computerSelection === "paper")) {
+        return "You won! ${playeSelection} beats ${computerSelection}."
+    }else{
+        return "You lose! ${computerSelection} beats ${playeSelection}."
+    }
+}
