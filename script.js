3e069e20dd618935eae183247a149cba52f2dbca
mxodaiy <m.khodaiy96@gmail.com>
2023-08-09 10:13:05 +0330

    added overall game function

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ function getComputerChoice(){
     return choices[i]
 }
 
-function battle(computerSelection, playerSelection){
+
+
+function playRound(computerSelection, playerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if (computerSelection === playerSelection){
-        return "Draw!"
+        return `Draw!`
     } else if ((playerSelection === "rock" && computerSelection === "scissors")
     || (playerSelection === "paper" && computerSelection === "rock")
     || (playerSelection === "scissors" && computerSelection === "paper")) {
-        return "You won! ${playeSelection} beats ${computerSelection}."
+        return `You won! ${playerSelection} beats ${computerSelection}.`
     }else{
-        return "You lose! ${computerSelection} beats ${playeSelection}."
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+function game(){
+    nRounds = parseInt(prompt("How many games do you want to play?",5))
+    for (let i = 1; i <= nRounds; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper or Scissors?");
+        console.log(playRound(computerSelection,playerSelection));
     }
 }
