bcf1ae74c04b3009a9e61311e9c8158f00b570d3
mxodaiy <m.khodaiy96@gmail.com>
2023-08-26 16:51:18 +0330

    add graphical representation of each round

--- script.js
+++ script.js
@@ -2,48 +2,65 @@ let playerScoreBoard =  document.querySelector('#result>.player>.score');
 let machineScoreBoard = document.querySelector('#result>.machine>.score');
 let drawBoard =         document.querySelector('#result>.draw>.score');
 
+let imgPlayerHand =  document.querySelector('#battleField>.player');
+let imgMachineHand = document.querySelector('#battleField>.machine');
+
 function scoreBoard(playerScore, machineScore){
     playerScoreBoard.textContent = playerScore;
     machineScoreBoard.textContent = machineScore;
     drawBoard.textContent = nPlayed - playerScore - machineScore;
 }
 
-function refree(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if (computerSelection !== playerSelection){
-        if ((playerSelection === "rock" && computerSelection === "scissors")
-        || (playerSelection === "paper" && computerSelection === "rock")
-        || (playerSelection === "scissors" && computerSelection === "paper")) {
+function refree(playerSelection, machineSelection){
+    imgPlayerHand.src =  `./img/${playerSelection}.svg`;
+    imgMachineHand.src = `./img/${machineSelection}.svg`;
+
+    imgPlayerHand.classList.remove('animateWave')
+    imgMachineHand.classList.remove('animateWave')
+
+    imgPlayerHand.classList.add('hand', `${playerSelection}`)
+    imgMachineHand.classList.add('hand', `${machineSelection}`)
+
+    if (machineSelection !== playerSelection){
+        if ((playerSelection === "rock" && machineSelection === "scissors")
+        || (playerSelection === "paper" && machineSelection === "rock")
+        || (playerSelection === "scissors" && machineSelection === "paper")) {
+            imgPlayerHand.classList.add('animateWave')
             playerScore++            
         }else{
+            imgMachineHand.classList.add('animateWave')
             machineScore++
         }
     }
+    nPlayed++;
     scoreBoard(playerScore, machineScore)
 }
 
 function getComputerChoice(){
-    choices = ["Rock", "Paper","Scissors"];
+    choices = ["rock", "paper","scissors"];
     i = Math.floor(Math.random()*3);
     return choices[i]
 }
 
 function playRound(playerSelection){
-    nPlayed++;
-    computerSelection = getComputerChoice();
-    refree(playerSelection, computerSelection);
-    if (nPlayed === nRounds) { gameOver(nRounds) }
+    machineSelection = getComputerChoice();
+    refree(playerSelection, machineSelection);
+    if (nPlayed >= nRounds){
+        gameOver(nRounds)
+    }
 }
 
 function gameOver(nRounds){
     if (playerScore > machineScore){
-        document.querySelector('#result>.player').classList.add('animateResult');
+        document.querySelector('#result>.player').classList.add('animateBlink');
     }else if (playerScore < machineScore){
-        document.querySelector('#result>.machine').classList.add('animateResult');
+        document.querySelector('#result>.machine').classList.add('animateBlink');
     }else{
-        document.querySelector('#result>.draw').classList.add('animateResult');
+        document.querySelector('#result>.draw').classList.add('animateBlink');
     }
+
+    imgPlayerHand.classList.remove('animateWave')
+    imgMachineHand.classList.remove('animateWave')
 }
 
 ['rock','paper','scissors'].forEach((hand) =>{
@@ -60,7 +77,14 @@ document.getElementById('play').addEventListener('click',() => {
     scoreBoard(playerScore, machineScore);
 
     ['player','machine','draw'].forEach((item) =>
-        document.querySelector(`#result>.${item}`).classList.remove('animateResult'));
+        document.querySelector(`#result>.${item}`).classList.remove('animateBlink'))
+
+    imgPlayerHand.src  = '';
+    imgMachineHand.src = '';
+    ['rock','paper','scissors'].forEach(item =>{
+        imgPlayerHand.classList.remove('hand', `${item}`)
+        imgMachineHand.classList.remove('hand', `${item}`)
+    })
 
     nRounds = parseInt(document.getElementById('nRounds').value);
 });
